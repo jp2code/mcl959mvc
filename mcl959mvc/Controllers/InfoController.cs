9106fa7527ecb274a7ee48fe3fdc80bb5e92bddf
jp2code <tjpool@gmail.com>
2026-08-26 15:37:05 -0500

    Added pages for Gun Raffle, Toys for Tots, Spaghetti Feed, and Building Maintenance.

---
+++ mcl959mvc/Controllers/InfoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mcl959mvc.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ToysForTots() => View();
+
+        public IActionResult SpaghettiFeed() => View();
+
+        public IActionResult GunRaffle() => View();
+
+        public IActionResult Maintenance() => View();
+    }
+}
