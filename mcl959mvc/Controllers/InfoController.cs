7c28262841e3c40ab2bb081c19f72e7913f34adc
jp2code <tjpool@gmail.com>
2026-08-27 15:51:29 -0500

    Added a placemat ad, verbiage in the Info views, and pulled member names for the Maintenance view.

--- mcl959mvc/Controllers/InfoController.cs
+++ mcl959mvc/Controllers/InfoController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using mcl959mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace mcl959mvc.Controllers
 {
     public class InfoController : Controller
     {
+        private readonly Mcl959DbContext _db;
+
+        public InfoController(Mcl959DbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -15,6 +24,26 @@ namespace mcl959mvc.Controllers
 
         public IActionResult GunRaffle() => View();
 
-        public IActionResult Maintenance() => View();
+        public async Task<IActionResult> Maintenance() {
+            var commandant = await _db.MemberRanks
+                .Where(r => r.DisplayRank == "Commandant")
+                .Join(_db.Roster,
+                    rank => rank.MemberNumber,
+                    member => member.MemberNumber,
+                    (rank, member) => member)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+            ViewBag.Commandant = commandant; // null if none found
+            var buildingSuper = await _db.MemberRanks
+                .Where(r => r.DisplayRank == "Building Superintendent")
+                .Join(_db.Roster,
+                    rank => rank.MemberNumber,
+                    member => member.MemberNumber,
+                    (rank, member) => member)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+            ViewBag.BuildingSuuper = buildingSuper;
+            return View();
+        }
     }
 }
